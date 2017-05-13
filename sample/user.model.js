1ea01b293da9e9cebf7c55f946949ca81b2ba4d6
Layton Whiteley <layton.whiteley@gmail.com>
2017-05-12 23:17:02 -0500

    format sample code

--- sample/user.model.js
+++ sample/user.model.js
@@ -2,30 +2,32 @@
 
 const mongoose = require('mongoose-fill');
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: false,
-    index: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-}, {
-  timestamps: {
-    createdAt: 'createdAt',
-    updatedAt: 'updatedAt',
-  },
-});
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: false,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  }, 
+
+  {
+    timestamps: {
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt',
+    },
+  }
+);
 
 module.exports = {
     schema: mongoose.model('User', UserSchema),
-    options: {
-
-    }
+    options: {}
 };
