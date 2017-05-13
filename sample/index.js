04ede76ced2b7fca8aa405089131b6d9d9913e95
Layton Whiteley <layton.whiteley@gmail.com>
2017-05-12 23:34:20 -0500

    style example

--- sample/index.js
+++ sample/index.js
@@ -19,7 +19,14 @@ app.use(api.middleware())
 
 app.listen(4444, () => {
   console.log('Open http://localhost:4444 and try')
+  var basePath = '';
   api.routes.forEach((route) => {
+    var base = route.path.split('/')[1]
+    if(basePath !== base){
+      var underline = '\n======================================='
+      console.log('\n\t\t', base, underline)
+      basePath = base;
+    }
     console.log(`${route.method} http://localhost:4444${route.path}`)
   })
 })
