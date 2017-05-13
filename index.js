bf0efe3a43fa8998b71400614e153db2c978233e
Layton Whiteley <layton.whiteley@gmail.com>
2017-05-13 00:22:06 -0500

    chore: add hidden and read only config

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const Router = require('koa-rest-router');
-const forEach = require('lodash/forEach');
+const assign = require('lodash/assign');
 const ctrlFactory = require('./lib/ctrl-factory')
 
 function KoaRestful (options = {}) {
   if (!(this instanceof KoaRestful)) {
     return new KoaRestful(options)
   }
+  const defaultOptions = {
+      router: {},
+      models: [],
+      defaultHiddenFields: ["__v", "password"],
+      defaultReadOnlyFields: ["_id", "id", "__v", "createdAt", "updatedAt", "password"]
+  };
 
   this.errHandler = options.errorHandler || ((err) => err);
+  this.options = assign({}, defaultOptions, options);
 
-  this.api = new Router(options.router || {});
-  forEach(options.models, (model) => {
+  this.api = new Router(this.options.router || {});
+  this.options.models.forEach((model) => {
     const ctrl = ctrlFactory(model, this);
     const modelName = model.schema.modelName.toLowerCase();
     this.api.resource(modelName, ctrl, model.options || {});
